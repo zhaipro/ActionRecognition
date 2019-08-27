c2175285c533922fec6ab30da79d1483b5c362e2
lixin <lixin@moviebook.cn>
2019-08-27 14:25:45 +0800

    工程化

--- src/main.py
+++ src/main.py
@@ -1,51 +1,58 @@
 import numpy as np
-
 import tcn
+from keras import models
+from keras.callbacks import ReduceLROnPlateau
+from keras.layers import Dense, Activation
 
 
-'''
+def preproccess(keypoints, actions):
+    # 动作的结束点减起始点，从而获取每一个动作的长度
+    lenght = actions[..., 1] - actions[..., 0]
+    # 过滤掉太长的和太短的，甚至长度为负数的动作…
+    actions = actions[np.logical_and(4 < lenght, lenght < 64)]
+    # 将其翻译成方便机器学习的数据结构
+    n = keypoints.shape[0]
+    y = np.zeros(n, dtype='int32')
+    w = np.zeros(n, dtype='float32')
+    for i, (start, last, action) in enumerate(actions):
+        for j in range(start, last + 1):
+            y[j] = action
+            w[j] = (j - start + 1) / (last - start + 1)
+
+    # 开始进行真正的预处理
+    epsilon = 1e-7
+    # 忽略置信度
+    x = keypoints[..., :2].copy()
+    x -= x.mean(1, keepdims=True)
+    x /= x.max(1, keepdims=True) - x.min(1, keepdims=True) + epsilon
+
+    return x, y, w
+
+
+def build_model(x, y):
+    i = models.Input(batch_shape=(None, x.shape[1], x.shape[2]))
+    o = tcn.TCN(return_sequences=True)(i)
+    o = Dense(y.max() + 1)(o)
+    o = Activation('softmax')(o)
+    m = models.Model(i, o)
+    return m
+
+
 data = np.load('actions.npz')
-keypoints = data['keypoints']
-actions = data['actions']
-print(actions.shape)
-lenght = actions[..., 1] - actions[..., 0]
-actions = actions[np.logical_and(4 < lenght, lenght < 64)]
-print(actions.shape)
-n = keypoints.shape[0]
-y = np.zeros(n, dtype='int32')
-w = np.zeros(n, dtype='float32')
-for i, (start, last, action) in enumerate(actions):
-    for j in range(start, last + 1):
-        y[j] = action
-        w[j] = (j - start + 1) / (last - start + 1)
-print(y.shape, w.shape)
-np.savez('actions.v2.npz', x=keypoints, y=y, w=w, labels=data['labels'])
-'''
-data = np.load('actions.v2.npz')
-x = data['x'][..., :2]
-y = data['y']
-w = data['w']
 l = data['labels']
-
-print(x.shape)
-print(y.shape)
-print(w.shape)
-x -= x.mean(1).reshape(-1, 1, 2)
-x /= (x.max(1) - x.min(1) + 0.000001).reshape(-1, 1, 2)
-i = 1404
-print(x[i])
-print('y =', l[y[i]], ';w =', w[i])
-x = x.reshape((1, -1, 12))
+x, y, w = preproccess(data['keypoints'], data['actions'])
+print(x.shape, y.shape, w.shape)
+x.shape = 1, -1, 12
 y.shape = 1, -1, 1
 w.shape = 1, -1
 
-from keras.models import Input, Model
-from keras.layers import Dense, Activation, BatchNormalization
-i = Input(batch_shape=(None, x.shape[1], 12))
-o = tcn.TCN(return_sequences=True)(i)
-o = Dense(y.max()+1)(o)
-o = Activation('softmax')(o)
-m = Model(i, o)
-m.compile(optimizer='adam', loss='sparse_categorical_crossentropy',  sample_weight_mode="temporal")
-m.fit(x, y, sample_weight=w, epochs=10)
-m.save('first.h5', include_optimizer=False)
+# m = build_model(x, y)
+m = models.load_model('first.h5')
+# 当标准评估停止提升时，降低学习速率
+reduce_lr = ReduceLROnPlateau(verbose=1)
+m.compile(optimizer='adam',
+          loss='sparse_categorical_crossentropy',
+          sample_weight_mode='temporal')
+m.fit(x, y, sample_weight=w, epochs=1600, callbacks=[reduce_lr])
+# m.save('first.h5', include_optimizer=False)
+m.save('first.h5')
