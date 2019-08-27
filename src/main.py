e430f27ed835ffd26f82eb2e6c9c8bc5587d0b74
lixin <lixin@moviebook.cn>
2019-08-27 11:49:12 +0800

    第一个能学习的模型

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-# import tcn
+import tcn
 
 
+'''
 data = np.load('actions.npz')
 keypoints = data['keypoints']
 actions = data['actions']
@@ -19,3 +20,32 @@ for i, (start, last, action) in enumerate(actions):
         w[j] = (j - start + 1) / (last - start + 1)
 print(y.shape, w.shape)
 np.savez('actions.v2.npz', x=keypoints, y=y, w=w, labels=data['labels'])
+'''
+data = np.load('actions.v2.npz')
+x = data['x'][..., :2]
+y = data['y']
+w = data['w']
+l = data['labels']
+
+print(x.shape)
+print(y.shape)
+print(w.shape)
+x -= x.mean(1).reshape(-1, 1, 2)
+x /= (x.max(1) - x.min(1) + 0.000001).reshape(-1, 1, 2)
+i = 1404
+print(x[i])
+print('y =', l[y[i]], ';w =', w[i])
+x = x.reshape((1, -1, 12))
+y.shape = 1, -1, 1
+w.shape = 1, -1
+
+from keras.models import Input, Model
+from keras.layers import Dense, Activation, BatchNormalization
+i = Input(batch_shape=(None, x.shape[1], 12))
+o = tcn.TCN(return_sequences=True)(i)
+o = Dense(y.max()+1)(o)
+o = Activation('softmax')(o)
+m = Model(i, o)
+m.compile(optimizer='adam', loss='sparse_categorical_crossentropy',  sample_weight_mode="temporal")
+m.fit(x, y, sample_weight=w, epochs=10)
+m.save('first.h5', include_optimizer=False)
