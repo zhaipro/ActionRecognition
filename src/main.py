66bdefcf423032a99993906e413586bd5875902f
lixin <lixin@moviebook.cn>
2019-08-30 14:23:14 +0800

    洗牌

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import tcn
 from keras import models
 from keras.layers import Dense, Activation
+from keras.utils import Sequence
 
 
 def preproccess(keypoints, actions):
@@ -27,7 +28,39 @@ def preproccess(keypoints, actions):
     x -= x.mean(1, keepdims=True)
     x /= x.max(1, keepdims=True) - x.min(1, keepdims=True) + epsilon
 
+    # 定义：自然放下的结束点是可以切割的点，从而方便对动作进行洗牌
+    # https://docs.scipy.org/doc/numpy/reference/generated/numpy.split.html
+    cut = actions[actions[:, 2] == 2][:, 1]
+    x = np.split(x, cut)
+    y = np.split(y, cut)
+    w = np.split(w, cut)
+
     return x, y, w
+
+
+# https://keras.io/zh/utils/#sequence
+# https://github.com/keras-team/keras/issues/9707
+class Generator(Sequence):
+    # Class is a dataset wrapper for better training performance
+    def __init__(self, x, y, w):
+        self.x, self.y = np.array(x), np.array(y)
+        self.w = np.array(w)
+        self.indices = np.arange(len(self.x))
+
+    def __len__(self):
+        return 1
+
+    def __getitem__(self, idx):
+        x = np.concatenate(self.x[self.indices])
+        y = np.concatenate(self.y[self.indices])
+        w = np.concatenate(self.w[self.indices])
+        x.shape = 1, -1, 12
+        y.shape = 1, -1, 1
+        w.shape = 1, -1
+        return x, y, w
+
+    def on_epoch_end(self):
+        np.random.shuffle(self.indices)
 
 
 def build_model(x, y):
@@ -41,27 +74,25 @@ def build_model(x, y):
 
 def _load_data(data):
     x, y, w = preproccess(data['keypoints'], data['actions'])
-    print('data.shape:', x.shape, y.shape, w.shape)
-    n = int(x.shape[0] * 0.9)
-    x_test = x[n:]
-    y_test = y[n:]
-    w_test = w[n:]
-    x = x[:n]
-    y = y[:n]
-    w = w[:n]
-    x.shape = 1, -1, 12
-    y.shape = 1, -1, 1
-    w.shape = 1, -1
+    n = int(len(x) * 0.9)
+    x_train = x[:n]
+    y_train = y[:n]
+    w_train = w[:n]
+
+    x_test = np.concatenate(x[n:])
+    y_test = np.concatenate(y[n:])
+    w_test = np.concatenate(w[n:])
     x_test.shape = 1, -1, 12
     y_test.shape = 1, -1, 1
     w_test.shape = 1, -1
-    return (x, y, w), (x_test, y_test, w_test)
+
+    return (x_train, y_train, w_train), (x_test, y_test, w_test)
 
 
 def evaluate():
     data = np.load('actions.npz')
     _, (x_test, y_test, w_test) = _load_data(data)
-    m = models.load_model('model.1566964020.h5')
+    m = models.load_model('model.1567145427.h5')
     m.compile(optimizer='rmsprop',
               loss='sparse_categorical_crossentropy',
               sample_weight_mode='temporal')
@@ -74,10 +105,14 @@ if __name__ == '__main__':
     exit()
     data = np.load('actions.npz')
     (x, y, w), (x_test, y_test, w_test) = _load_data(data)
-    m = build_model(x, y)
+    m = build_model(x_test, y_test)
     m.compile(optimizer='rmsprop',
               loss='sparse_categorical_crossentropy',
               sample_weight_mode='temporal')
-    m.fit(x, y, sample_weight=w, epochs=1600,
-          validation_data=(x_test, y_test, w_test))
+
+    train_datagen = Generator(x, y, w)
+    m.fit_generator(train_datagen, epochs=1600,
+                    steps_per_epoch=1,
+                    validation_data=(x_test, y_test, w_test))
+
     m.save(f'model.{int(time.time())}.h5', include_optimizer=False)
